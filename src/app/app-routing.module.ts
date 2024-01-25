8d405461686456bcac82ddcf0e15ef9fcb2563cf
Florentinanalista <147798634+Florentinanalista@users.noreply.github.com>
2024-01-25 11:19:29 +0700

    UAS212102435
    UAS212102435

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'mahasiswa', pathMatch: 'full'},
+  {path: 'mahasiswa', component: MahasiswaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
